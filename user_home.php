4c14b65d05ec23abf417c3c885ff59feeef31e56
Heath-Mayocchi <hmayocchi@gmail.com>
2017-07-28 19:19:14 +1000

    user home updated

---
+++ user_home.php
@@ -0,0 +1,37 @@
+<!--
+QUT Capstone Project 2017
+Project Owner: Nursery Road State Special School
+
+SNAP - Social Networking Action Platform
+
+Author: Robert Piper
+Author: Heath Mayocchi
+Author: Levinard Hugo
+Author: David MacKenzie
+-->
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" type="text/css" href="CSS/main.css">
+	<title>SNAP HOME</title>
+</head>
+<body class="user_home">
+	<h1>SNAP</h1>
+	<h2>Social Networking Action Platform</h2>
+	<div class="home_button">
+		<p><a href ="feed.html"></a><div id ="view_feed_text">VIEW<br>FEED</div></p>
+	</div>
+	<div class="home_button">
+		<p><a href ="post.html"></a><div id ="make_post_text">CREATE<br>POST</div></p>
+	</div>
+	<div class="home_button">
+		<p><a href ="messages.html"></a><div id ="messages_text">MESSAGES</div></p>
+	</div>
+	<div class="home_button">
+		<p><a href="friends.html"></a><div id ="friends_text">FRIENDS</div></p>
+	</div>
+</body>
+</html>
