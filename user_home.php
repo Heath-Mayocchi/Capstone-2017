581ee9a0a4eec9ba87daffc328f089c04b16ce43
DaveMack <DaveMack@users.noreply.github.com>
2017-08-06 17:12:42 +1000

    php for front end carousel.
    Just altered user_home.php to link to the view_feed.php

--- user_home.php
+++ user_home.php
@@ -17,7 +17,32 @@ Author: David MacKenzie
 	<meta charset="utf-8"/>
 	<link rel="stylesheet" type="text/css" href="css/user_home.css">
 	<script src="js/user_home.js"></script>
+	<script src="js/shortcut.js" type="text/javascript"></script>
 	<title>SNAP HOME</title>
+	<script type="text/javascript">
+	function init() 
+	{
+		var homeElements = document.getElementsByClassName("button");
+		var currentElement = 6;
+
+		shortcut.add("right", function() 
+		{
+			if(currentElement==homeElements.length-1)
+			{
+				currentElement = 0;
+			}else
+			{
+				currentElement++;
+			}
+			homeElements[currentElement].focus();
+
+		});
+		shortcut.add("enter", function() {
+			alert(document.activeElement.id);
+		});
+	}
+	window.onload=init;
+	</script>	
 </head>
 <body class="user_home_wrapper">
 	<header>
@@ -30,15 +55,15 @@ Author: David MacKenzie
 	<h2>Social Networking Action Platform</h2>
 	</header>
 	<section>
-	<button class="button btnsquare" id="view_feed_btn" onclick="location.href='#';"><div id="view_feed_text">VIEW<br>FEED</div></button>
+	<button class="button btnsquare" id="view_feed_btn" onclick="location.href='view_feed.php';"><div id="view_feed_text">VIEW<br>FEED</div></button>
 	<button class="button btnsquare" id="create_post_btn" onclick="location.href='#';"><div id ="make_post_text">CREATE<br>POST</div></button>
 	<button class="button btnsquare" id="messages_btn" onclick="location.href='#';"><div id ="messages_text">MESSAGES</div></button>
 	<button class="button btnsquare" id="friends_btn" onclick="location.href='#';"><div id ="friends_text">FRIENDS</div></button>
 	</section>
-	<footer>
-	<button class="previous_btn" onkeypress="userHomeBtnFocusBack(event)"><img src="img/previous.png" alt="Previous"></img></button>
-	<button class="next_btn" autofocus onfocus="nextfocus()" onkeypress="userHomeBtnFocusFwd(event)"><img class="next_btn_img" src="img/next.png" alt="Next"></img></button>
-	<button class="button select_btn" onkeypress="userHomeBtnActivate(event)">Select</button>
+	<footer >
+	<button class="button previous_btn" id="previous_btn"><img src="img/previous.png" alt="Previous"></img></button>
+	<button class="button next_btn" id="next_btn" autofocus onfocus="nextfocus()"><img class="next_btn_img" src="img/next.png" alt="Next"></img></button>
+	<button class="button select_btn" id="select_btn">Select</button>
 	</footer>
 	</body>
 </html>
