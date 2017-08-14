58e00fc9c86491a0526ef55523fdac1fccef2870
Levi Hugo <levinard.hugo@connect.qut.edu.au>
2017-08-14 22:50:42 +1000

    Finished the css and javascript for the effect when a picture is selected, this page should now be completed front-end wise

--- js/postPreview.js
+++ js/postPreview.js
@@ -1,3 +1,4 @@
+/*	Element selectors	*/
 var pictureButton = document.querySelector(".pictureButton");
 var textArea = document.querySelector("textarea");
 var modal = document.querySelector(".modal");
@@ -5,12 +6,12 @@ var images = document.querySelectorAll(".modal img");
 var displayPic = document.querySelector("#selectedPicture");
 var selectButton = document.querySelector(".selectPicButton");
 
+//	 Used for conditions	
 var theSource;
-var compareSource;
-var compareSource2;
 var isTrue = false;
+var midas = 0;
 
-
+//	Button for the picture to show the 6 pictures	
 pictureButton.addEventListener("click", function() {
 	if (isTrue == false) {
 		textArea.classList.toggle("textBig");
@@ -19,6 +20,7 @@ pictureButton.addEventListener("click", function() {
 	modal.classList.toggle("hideModal");
 });
 
+//	Select button for the pictures 
 selectButton.addEventListener("click", function() {
 	displayPic.src = theSource;
 	modal.classList.add("hideModal");
@@ -26,10 +28,31 @@ selectButton.addEventListener("click", function() {
 	isTrue = true;
 });
 
+//	For getting the clicked image src	
 for (var i = 0; i < images.length; i++) {
 	images[i].addEventListener("click", function () {
 		theSource = this.src;
 	});
 }
 
+//	For adding the id when a specific image is clicked 
+for (var i = 0; i < images.length; i++) {
+	images[i].addEventListener("click", function() {
+		if (midas === 0) {
+			resetColor();
+			this.id = (this.id == "imageSelected") ? "" : "imageSelected";
+			midas++;
+		} else if (midas === 1) {
+			resetColor();
+			this.id = (this.id == "imageSelected") ? "" : "imageSelected";
+			midas = 0;	
+		}
+	});
+}	
 
+//	Used for resetting	
+function resetColor() {
+	for (var i = 0; i < images.length; i++) {
+		images[i].removeAttribute("id");
+	}
+};
