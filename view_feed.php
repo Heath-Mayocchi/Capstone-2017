3288bba9e90e6685b38e01f2f0ac152da34d0a71
Heath-Mayocchi <hmayocchi@gmail.com>
2017-08-14 17:57:18 +1000

    comment buttons fixed

--- view_feed.php
+++ view_feed.php
@@ -190,8 +190,8 @@ Author: David MacKenzie
 	<span id="comment_box" class="hidden">
 		<textarea rows="4" cols="50" id="comment_form" form="usrform">Enter comment... (max 32 characters)
 		</textarea>
-		<button type="submit" id="submit_button">Submit</button>
-		<button type="submit" id="cancel_button">Cancel</button>
+		<button class="button" type="submit" id="submit_button">Submit</button>
+		<button class="button" type="submit" id="cancel_button">Cancel</button>
 	</span>
 
 	<footer >
