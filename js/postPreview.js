709bdd5ef8e1f8bebfebdd2cf49b54c3816e0ff4
Levi Hugo <levinard.hugo@connect.qut.edu.au>
2017-08-18 12:45:31 +1000

    Refactored the postPreview.js to make it look cleaner and easier to read

--- js/postPreview.js
+++ js/postPreview.js
@@ -15,79 +15,110 @@ var emojiLove = document.querySelector("#emoji_love");
 var emojiLaugh = document.querySelector("#emoji_laugh");
 var emojiWow = document.querySelector("#emoji_wow");
 var emojiSad = document.querySelector("#emoji_sad");
-
-emojiLike.addEventListener("click", function() {
-	var temp = String.fromCodePoint(0x1f44d);	// unicode for thumbs up
-	textArea.value += temp;
-});
-
-emojiLove.addEventListener("click", function() {
-	var temp = String.fromCodePoint(0x2764);	// unicode for heart
-	textArea.value += temp;
-});
-
-emojiLaugh.addEventListener("click", function() {
-	var temp = String.fromCodePoint(0x1F600);	// unicode for grinning 
-	textArea.value += temp;
-});
-
-emojiWow.addEventListener("click", function() {
-	var temp = String.fromCodePoint(0x1F62E);	// unicode fro wow
-	textArea.value += temp;
-});
-
-emojiSad.addEventListener("click", function() {
-	var temp = String.fromCodePoint(0x1F622);	// unicode for sad
-	textArea.value += temp;
-});
-
-//	 Used for conditions	
+	
 var theSource;
 var isTrue = false;
 
-//	Button for the picture to show the 6 pictures	
-pictureButton.addEventListener("click", function() {
+mainFunc();	// Calls main function
+
+
+/* functions below	
+
+	- Levi
+*/
+
+function resetColor() {
+	//	Used for resetting	
+	for (var i = 0; i < images.length; i++) {
+		images[i].removeAttribute("id");
+	}
+};
+
+function pictureButtonFunc() {
+	// Used for toggling the textarea to be small or big depending if the user wants to use a picture
 	if (isTrue == false) {
 		textArea.classList.toggle("textBig");
 		textArea.classList.toggle("textSmall");
 	}
 	modal.classList.toggle("hideModal");
-});
+}
 
-//	Select button for the pictures 
-selectButton.addEventListener("click", function() {
+function selectButtonFunc() {
+	// Function for the select button, which is the button that opens along with the modal
 	displayPic.src = theSource;
 	modal.classList.add("hideModal");
 	displayPic.style.display = "block";
 	isTrue = true;
-});
-
-emojiButton.addEventListener("click", function() {
-	emojiSelection.style.visibility = "visible";
-});
-
-emojiCancelButton.addEventListener("click", function() {
-	emojiSelection.style.visibility = "hidden";
-});
-
-//	For getting the clicked image src	
-for (var i = 0; i < images.length; i++) {
-	images[i].addEventListener("click", function () {
-		theSource = this.src;
-	});
 }
 
-//	For adding the id when a specific image is clicked 
-for (var i = 0; i < images.length; i++) {
-	images[i].addEventListener("click", function() {
-			resetColor();
-			this.id = (this.id == "imageSelected") ? "" : "imageSelected";
-	});
-}	
+function emojiButtonFunc() {
+	// Shows the emoji selection when the emoji button is clicked
+	emojiSelection.style.visibility = "visible";
+}
 
-//	Used for resetting	
-function resetColor() {
-	for (var i = 0; i < images.length; i++) {
-		images[i].removeAttribute("id");
+function emojiCancelButtonFunc() {
+	// Hides the emoji selection when the cancel button is clicked
+	emojiSelection.style.visibility = "hidden";
+}
+
+function emote(emotion) {
+	// Adds an emoji to the form when a specific emoji is clicked
+	switch(emotion) {
+		case 'like':
+			textArea.value += String.fromCodePoint(0x1f44d);
+			break;
+		case 'heart':
+			textArea.value += String.fromCodePoint(0x2764);
+			break;
+		case 'grin':
+			textArea.value += String.fromCodePoint(0x1F600);
+			break;
+		case 'wow':
+			textArea.value += String.fromCodePoint(0x1F62E);
+			break;
+		case 'sad':
+			textArea.value += String.fromCodePoint(0x1F622);
+			break;
 	}
-};
+}
+
+
+
+/* This is the main function, which runs everything on the page.
+   I decided to put everything inside one function to make it cleaner, and easier to read.. */
+function mainFunc() {
+	//	For getting the clicked image src	
+	for (var i = 0; i < images.length; i++) {
+		images[i].addEventListener("click", function () {
+			theSource = this.src;
+		});
+	}
+	//	For adding the id when a specific image is clicked 
+	for (var i = 0; i < images.length; i++) {
+		images[i].addEventListener("click", function() {
+				resetColor();
+				this.id = (this.id == "imageSelected") ? "" : "imageSelected";
+		});
+	}	
+
+	// Event listeners
+	pictureButton.addEventListener("click", pictureButtonFunc);			//	Button for the picture to show the 6 pictures	
+	selectButton.addEventListener("click", selectButtonFunc);			//	Select button for the pictures 
+	emojiButton.addEventListener("click", emojiButtonFunc);				//  Emoji button
+	emojiCancelButton.addEventListener("click", emojiCancelButtonFunc);	// closes the emoji selection
+	emojiLike.addEventListener("click", function() {
+		emote("like");
+	});
+	emojiLove.addEventListener("click", function() {
+		emote("heart");
+	});
+	emojiLaugh.addEventListener("click", function() {
+		emote("grin");
+	});
+	emojiWow.addEventListener("click", function() {
+		emote("wow");
+	});
+	emojiSad.addEventListener("click", function() {
+		emote("sad");
+	});
+}
