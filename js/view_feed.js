a8ca42290dd14c7689721a563fd9117487b2360c
DaveMack <d1.mackenzie@connect.qut.edu.au>
2017-08-22 13:26:17 +1000

    feed backend work

--- js/view_feed.js
+++ js/view_feed.js
@@ -1,7 +1,13 @@
+// var switchBtn = 32;
+// var selectBtn = 13;
+
+var switchBtn = 39;
+var selectBtn = 38;
+
 function init() 
 {
 	showPost(slideIndex);
-	showComment(commentSlider);
+	showLatestComment(commentSlider);
 }
 window.onload=init;
 
@@ -37,10 +43,10 @@ backward.addEventListener("click", function() {
 });
 
 function plusComment(n) {
-	showComment(commentSlider += n);
+	showLatestComment(commentSlider += n);
 }
 
-function showComment(n) {
+function showLatestComment(n) {
 	var items = document.getElementsByClassName("latestCommentBar");
 	var length = items.length;
 	if (n > length) { 
@@ -55,92 +61,298 @@ function showComment(n) {
 	items[commentSlider - 1].style.display = "block";
 }
 
+// function for back key presses
+function feedBtnBack(event){
+    var key = event.which;
+	// if key pressed is the right arrow, change focus to 'react' footer button
+	if (key == switchBtn){
+		document.getElementById("back_btn").blur();
+		document.getElementById("previous_btn").focus();
+	}
+	// if key pressed is the up arrow, view next post
+	if (key == selectBtn){
+		event.preventDefault();
+        event.stopPropagation();
+		window.history.back();
+	}	
+}
 
 // function for next button key presses
 function feedBtnNext(event) {	
     var key = event.which;
-	// if key pressed is the spacebar, change focus to 'react' footer button
-	if (key == 32){
+	// if key pressed is the right arrow, change focus to 'react' footer button
+	if (key == switchBtn){
 		document.getElementById("next_btn").blur();
 		document.getElementById("choose_btn").focus();
 	}
-	// if key pressed is the enter key, view next post
-	if (key == 13){  
-		key.preventDefault();
-		key.stopPropagation();
+	// if key pressed is the up arrow, view next post
+	if (key == selectBtn){        
+		event.preventDefault();
+        event.stopPropagation();
 		displayPost(-1);
 		plusComment(-1);
 	}
 }
 
+/* 
+function for selecting emojis
+
+hide react buttons
+display emoji selection
+prev and next change from react btn focus to emoji select focus 
+*/
+function feedBtnEmoji(event){
+    var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("emoji_button").blur();
+		document.getElementById("comment_button").focus();
+	}
+ if (key == selectBtn){
+		hideReactButtons();
+		showEmojiButtons();
+		document.getElementById("emoji_button").blur();
+		document.getElementById("emoji_like").focus();
+	}
+}
+/*
+function for selecting comment
+
+hide react buttons
+display comment form
+prev and next change from react btn focus to comment form focus
+*/
+function feedBtnComment(event){
+   var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("comment_button").blur();
+		document.getElementById("react_cancel_button").focus();		
+	}
+	// if (key == selectBtn){
+	// 	document.getElementById("react_buttons").style.visibility = "hidden";
+	// 	document.getElementById("comment_form").style.visibility = "visible";
+	// }	
+}
+
+function showNavButtons()
+{
+	// show nav buttons
+	document.getElementById("postNavigationButtons").style.display = "block";
+	document.getElementById("previous_btn").classList.add('visible'); 
+	document.getElementById("previous_btn").classList.remove('hidden'); 
+	document.getElementById("next_btn").classList.add('visible'); 
+	document.getElementById("next_btn").classList.remove('hidden'); 
+	document.getElementById("choose_btn").classList.add('visible'); 
+	document.getElementById("choose_btn").classList.remove('hidden'); 
+}
+
+function hideNavButtons()
+{
+	// hide nav buttons
+	document.getElementById("postNavigationButtons").style.display = "none";
+	document.getElementById("previous_btn").classList.add('hidden'); 
+	document.getElementById("previous_btn").classList.remove('visible'); 
+	document.getElementById("next_btn").classList.add('hidden'); 
+	document.getElementById("next_btn").classList.remove('visible'); 
+	document.getElementById("choose_btn").classList.add('hidden'); 
+	document.getElementById("choose_btn").classList.remove('visible'); 
+}
+
+function showReactButtons()
+{
+	document.getElementById("react_buttons").style.display = "block";
+	document.getElementById("emoji_button").classList.add('visible'); 
+	document.getElementById("emoji_button").classList.remove('hidden'); 
+	document.getElementById("comment_button").classList.add('visible'); 
+	document.getElementById("comment_button").classList.remove('hidden'); 
+	document.getElementById("react_cancel_button").classList.add('visible'); 
+	document.getElementById("react_cancel_button").classList.remove('hidden'); 		
+}
+function hideReactButtons()
+{
+	document.getElementById("react_buttons").style.display = "none";
+	document.getElementById("emoji_button").classList.add('hidden'); 
+	document.getElementById("emoji_button").classList.remove('visible'); 
+	document.getElementById("comment_button").classList.add('hidden'); 
+	document.getElementById("comment_button").classList.remove('visible'); 
+	document.getElementById("react_cancel_button").classList.add('hidden'); 
+	document.getElementById("react_cancel_button").classList.remove('visible'); 
+}
+
+function showEmojiButtons()
+{
+	document.getElementById("emoji_selection").style.display = "block";
+}
+function hideEmojiButtons()
+{
+	document.getElementById("emoji_selection").style.display = "none";
+}
+
+function showComments()
+{
+	document.getElementById("comment_view").classList.add('visible'); 
+	document.getElementById("comment_view").classList.remove('hidden'); 
+	document.getElementById("post_view").style.left = "0%"; 
+	document.getElementById("latestCommentSection").style.left = "0%"; 
+}
+
+function hideComments()
+{
+	document.getElementById("comment_view").classList.add('hidden'); 
+	document.getElementById("comment_view").classList.remove('visible'); 
+	document.getElementById("post_view").style.left = "20%"; 
+	document.getElementById("latestCommentSection").style.left = "18%"; 	
+}
+/*
+function for selecting cancel after react
+
+hide comments
+hide react buttons
+prev and next from comment form focus to changing post display
+change select button function
+*/
+function feedBtnCancel(event){ 
+   var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("react_cancel_button").blur();
+		document.getElementById("emoji_button").focus();		
+	}
+	else{
+		
+		if( key == selectBtn){
+
+			showNavButtons();
+			hideReactButtons();
+			hideComments();
+		
+			// auto focus next button		
+			document.getElementById("emoji_button").blur();
+			document.getElementById("next_btn").focus();	
+
+		}
+	}	
+}
+/*
+function for selecting cancel when selecting emojis
+
+hide emojis
+prev and next from selecting emoji focus to changing post display
+change select button function
+*/
+function emojiButtonCancel(event){ 
+  var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("emoji_cancel_btn").blur();
+		document.getElementById("emoji_like").focus();
+	}
+  if (key == selectBtn){
+		// hide emoji/ comment buttons	
+		hideEmojiButtons();
+		showReactButtons();
+
+		document.getElementById("emoji_cancel_btn").blur();
+		document.getElementById("emoji_button").focus();
+	}
+}
 
 // toggles the comment section from hidden to visible and shifts the post left when the comments are viewable
 function feedBtnClickChoose(){  
-	/* if (document.getElementById("comment_view").classList.contains('visible')) 
-	{ 
-		document.getElementById("comment_view").classList.add('hidden'); 
-		document.getElementById("comment_view").classList.remove('visible'); 
-		document.getElementById("post_view").style.left = "20%"; 
-		document.getElementById("latestCommentSection").style.left = "18%";
-		document.getElementById("reacted_emojis").style.marginLeft = "260px";
-	} else  
-	{ 
-		document.getElementById("comment_view").classList.add('visible'); 
-		document.getElementById("comment_view").classList.remove('hidden'); 
-		document.getElementById("post_view").style.left = "0%"; 
-		document.getElementById("latestCommentSection").style.left = "0%"; 
-		document.getElementById("reacted_emojis").style.marginLeft = "60px";
-	} */
-}		
 
-// variables to keep track of footer buttons
-var postNavButtons = true;
-var chooseButtons = false;	
-var emojiSelectButtons = false;
-var commentButtons = false;
+	hideNavButtons();
+	showReactButtons();
+
+	showComments();
+		
+	// auto focus emoji button		
+	document.getElementById("choose_btn").blur();
+	document.getElementById("emoji_button").focus();	
+}		
+		
 
 // function for choose button key presses
 function feedBtnChoose(event) {
-	var key = event.which;
-	// if key pressed is the spacebar, change focus to the back button
-	if (key == 32){ 
+		var key = event.which;
+		// if key pressed is the switchBtn, change focus to the back button
+		if (key == switchBtn){ 
 			document.getElementById("choose_btn").blur();
 			document.getElementById("back_btn").focus();		
-	}
-	// if key pressed is the enter key, display react buttons and comments
-	if (key == 13){
-		// hide nav buttons
-		document.getElementById("postNavigationButtons").style.display = "none";
-		// diplay emoji/ comment buttons
-		document.getElementById("choose_buttons").style.display = "block";
-		// display comments
-		document.getElementById("comment_view").classList.add('visible'); 
-		document.getElementById("comment_view").classList.remove('hidden'); 
-		document.getElementById("post_view").style.left = "0%"; 
-		document.getElementById("latestCommentSection").style.left = "0%"; 
-		document.getElementById("reacted_emojis").style.marginLeft = "60px";
-		// auto focus emoji button
-		document.getElementById("emoji_button").focus();
-		postNavButtons = false;
-		chooseButtons = true;
-	}	
+		}else 
+		{
+			if (key == selectBtn)
+			{
+				feedBtnClickChoose();
+			}			
+		}	// if key pressed is the selectBtn key, display react buttons and comments
+	
 }
 
 // function for previous button key presses
 function feedBtnPrevious(event) {
     var key = event.which;
 	// if key pressed is the spacebar, change focus to 'next' footer button
-	if (key == 32){ 	
+	if (key == switchBtn){ 	
 		document.getElementById("previous_btn").blur();
 		document.getElementById("next_btn").focus();
 	}
 	// if key pressed is the enter key, view previous post
-	if (key == 13){
-		key.preventDefault();
-		key.stopPropagation();
+	if (key == selectBtn){
+		event.preventDefault();
+        event.stopPropagation();
 		displayPost(1);	
 		plusComment(1); //this is for the latest comment
 	}
 }
 
 
+function emojiButtonLike(event)
+{
+  var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("emoji_like").blur();
+		document.getElementById("emoji_love").focus();
+	}
+}
+
+function emojiButtonLove(event)
+{
+  var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("emoji_love").blur();
+		document.getElementById("emoji_laugh").focus();
+	}
+}
+
+function emojiButtonLaugh(event)
+{
+  var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("emoji_laugh").blur();
+		document.getElementById("emoji_wow").focus();
+	}
+}
+
+function emojiButtonWow(event)
+{
+  var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("emoji_wow").blur();
+		document.getElementById("emoji_sad").focus();
+	}
+}
+
+function emojiButtonSad(event)
+{
+  var key = event.which;
+	// if key pressed is the switchBtn, change focus to next footer button
+	if (key == switchBtn){
+		document.getElementById("emoji_sad").blur();
+		document.getElementById("emoji_cancel_btn").focus();
+	}
+}
--- view_feed.php
+++ view_feed.php
@@ -10,8 +10,8 @@ Author: Levinard Hugo
 Author: David MacKenzie	
 -->
 
+<?php require "./php/view_feed.inc"; ?>
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
 	<meta charset="utf-8"/>
@@ -27,48 +27,13 @@ Author: David MacKenzie
 			<img src="img/profile-placeholder.png" alt="User profile image"></img>
 			<p>Super Long User Name</p>
 		</div>
-		<h2>DAVE TEST</h2>
+		<h2>SNAP FEED</h2>
 	</header>
 	
 	<section>
-		<div id="post_view">
-			<div class="post">
-				<img src="img/tank.jpg">
-				<div class="userPost">
-					<img src="img/profile1.jpg" class="userPic">
-					<div class="userComment">
-						Is 64 characters enough to say what I want, this could hold 100 but might cause some issues with size
-					</div>
-					<div class="userPostDate">
-						Abdul Muhammad&nbsp;1:52 am&nbsp;
-					</div>
-				</div>
-			</div>
-			<div class="post">
-				<img src="img/cheesecake.jpg">
-				<div class="userPost">
-					<img src="img/profile2.jpg" class="userPic">
-					<div class="userComment">
-						Lunch needs more of this. :)
-					</div>
-					<div class="userPostDate">
-						Victoria Justice&nbsp;11:03 am&nbsp;
-					</div>
-				</div>
-			</div>
-			<div class="post">
-				<img src="img/kitten.jpg">
-				<div class="userPost">
-					<img src="img/profile3.jpg" class="userPic">
-					<div class="userComment">
-						Mr Boots Nom Nom Nom!
-					</div>
-					<div class="userPostDate">
-						Corey Parkinson&nbsp;11:00 am&nbsp;
-					</div>
-				</div>
-			</div>
-		</div>
+
+		<?php view_feed();?>
+
 		<div id="comment_view" class="hidden">
 			<div class="comment">
 				<span class="comment_body">Testing 64 characters in the textbox for the feed page MMMM MMMM</p>
@@ -101,32 +66,6 @@ Author: David MacKenzie
 		</div>
 	</section>
 	
-	
-
-		<table id="reacted_emojis">
-			<tr>
-				<th>
-					<img class="emoji_img" id="emoji_like_reacted" src="img/emoji-like.png" alt="Like"></img>
-				</th>
-				<th>
-					<img class="emoji_img" id="emoji_love_reacted" src="img/emoji-love.png" alt="Love"></img>
-				</th>
-				<th>
-					<img class="emoji_img" id="emoji_laugh_reacted" src="img/emoji-laugh.png" alt="Laugh"></img>
-				</th>
-				<th>
-					<img class="emoji_img" id="emoji_wow_reacted" src="img/emoji-wow.png" alt="Wow"></img>
-				</th>
-				<th>
-					<img class="emoji_img" id="emoji_sad_reacted" src="img/emoji-sad.png" alt="Sad"></img>
-				</th>
-				<th>
-					<p id="reacted_emoji_txt">You and 12 others</p>
-				</th>
-			</tr>
-		</table>
-		
-		
 	<div id="latestCommentSection">
 		<p id="latestCommentLabel">Latest Comment</p>
 		<div class="latestCommentBar">
@@ -175,45 +114,45 @@ Author: David MacKenzie
 	
 	<article>
 		<div id="emoji_selection">
-			<table id="emoji_selection_table">
+			<table>
 				<tr>
 					<th>
 						<figure>
-							<button class="button emojiBtn" id="emoji_like" onkeyup="feedBtnLike(event)"><img class="emoji_img" id="emoji_like_img" src="img/emoji-like.png" alt="Like"></img></button>
+							<button class="button emojiBtn" id="emoji_like" type="submit" onkeyup="emojiButtonLike(event)"><img class="emoji_img" id="emoji_like_img" src="img/emoji-like.png" alt="Like"></img></button>
 							<!-- hide emoji selection, update reacted emojis, prev and next from emoji selection focus to changing post display  -->
-							<figcaption id="like">Like</figcaption>
+							<figcaption>Like</figcaption>
 						</figure>
 					</th>
 					<th>
 						<figure>
-							<button class="button emojiBtn" id="emoji_love" onkeyup="feedBtnLove(event)"><img class="emoji_img" id="emoji_love_img" src="img/emoji-love.png" alt="Love"></img></button>
+							<button class="button emojiBtn" id="emoji_love" type="submit" onkeyup="emojiButtonLove(event)"><img class="emoji_img" id="emoji_love_img" src="img/emoji-love.png" alt="Love"></img></button>
 							<!-- hide emoji selection, update reacted emojis, prev and next from emoji selection focus to changing post display  -->
-							<figcaption id="love">Love</figcaption>
+							<figcaption>Love</figcaption>
 						</figure>
 					</th>
 					<th>
 						<figure>
-							<button class="button emojiBtn" id="emoji_laugh" onkeyup="feedBtnLaugh(event)"><img class="emoji_img" id="emoji_laugh_img" src="img/emoji-laugh.png" alt="Laugh"></img></button>
+							<button class="button emojiBtn" id="emoji_laugh" type="submit" onkeyup="emojiButtonLaugh(event)"><img class="emoji_img" id="emoji_laugh_img" src="img/emoji-laugh.png" alt="Laugh"></img></button>
 							<!-- hide emoji selection, update reacted emojis, prev and next from emoji selection focus to changing post display  -->
-							<figcaption id="laugh">Laugh</figcaption>
+							<figcaption>Laugh</figcaption>
 						</figure>
 					</th>
 					<th>
 						<figure>
-							<button class="button emojiBtn" id="emoji_wow" onkeyup="feedBtnWow(event)"><img class="emoji_img" id="emoji_wow_img" src="img/emoji-wow.png" alt="Wow"></img></button>
+							<button class="button emojiBtn" id="emoji_wow" type="submit" onkeyup="emojiButtonWow(event)"><img class="emoji_img" id="emoji_wow_img" src="img/emoji-wow.png" alt="Wow"></img></button>
 							<!-- hide emoji selection, update reacted emojis, prev and next from emoji selection focus to changing post display  -->
-							<figcaption id="wow">Wow</figcaption>
+							<figcaption>Wow</figcaption>
 						</figure>
 					</th>
 					<th>
 						<figure>
-							<button class="button emojiBtn" id="emoji_sad" onkeyup="feedBtnSad(event)"><img class="emoji_img" id="emoji_sad_img" src="img/emoji-sad.png" alt="Sad"></img></button>
+							<button class="button emojiBtn" id="emoji_sad" type="submit" onkeyup="emojiButtonSad(event)"><img class="emoji_img" id="emoji_sad_img" src="img/emoji-sad.png" alt="Sad"></img></button>
 							<!-- hide emoji selection, update reacted emojis, prev and next from emoji selection focus to changing post display  -->
-							<figcaption id="sad">Sad</figcaption>
+							<figcaption>Sad</figcaption>
 						</figure>
 					</th>
 					<th>
-						<button class="button" id="emoji_cancel_btn" onkeyup="feedBtnEmojiCancel(event)">Cancel</button>
+						<button class="button" id="emoji_cancel_btn" onkeyup="emojiButtonCancel(event)">Cancel</button>
 						<!-- hide emoji selection, prev and next from emoji selection focus to changing post display  -->
 					</th>
 				</tr>
@@ -223,43 +162,38 @@ Author: David MacKenzie
 			<table>
 				<tr>
 					<th>
-						<textarea rows="2" id="comment_text_box" form="comment_on_post" placeholder="Enter comment... (max 90 characters)"></textarea>
+						<textarea rows="2" id="comment_text_box" form="comment_on_post" placeholder="Enter comment... (max 32 characters)"></textarea>
 					</th>
 					<th>
-						<button class="button" id="comment_submit_button" onkeydown="feedBtnCommentsubmit(event)">Submit</button>
+						<button class="button" id="comment_submit_button" type="submit">Submit</button>
 						<!-- hide comment form, update latest comment and comments list, prev and next from comment form focus to changing post display  -->
 					</th>
 					<th>
-						<button class="button" id="comment_cancel_button" onkeydown="feedBtnCommentCancel(event)">Cancel</button>
+						<button class="button" id="comment_cancel_button" onkeydown="feedBtnCommentCancel()">Cancel</button>
 						<!-- hide comment form, prev and next from comment form focus to changing post display  -->
 					</th>
 				</tr>
 			</table>
 		</div>
+
 	</article>
 
 	<footer>
-		<div id="choose_buttons">
-			<button class="button" id="emoji_button" onkeyup="feedBtnEmoji(event)">Emoji</button>
-			<button class="button" id="comment_button" onkeyup="feedBtnComment(event)">Comment</button>
-			<button class="button" id="choose_cancel_button" onkeyup="feedBtnChooseCancel(event)">Cancel</button>
-		</div>
 		<div id="postNavigationButtons">
 			<button class="button" id="previous_btn" onkeyup="feedBtnPrevious(event)" onclick="displayPost(-1)">Previous</button>
 			<button class="button" id="next_btn" autofocus onkeyup="feedBtnNext(event)" onclick="displayPost(1)">Next</button>
 			<button class="button" id="choose_btn" onkeyup="feedBtnChoose(event)" onclick="feedBtnClickChoose()">Choose</button>
 		</div>
+		<div id="react_buttons">
+			<button class="button" id="emoji_button" onkeyup="feedBtnEmoji(event)">Emoji</button>
+			<button class="button" id="comment_button" onkeyup="feedBtnComment(event)">Comment</button>
+			<button class="button" id="react_cancel_button" onkeyup="feedBtnCancel(event)">Cancel</button>
+		</div>
 	</footer>
 	
 	<!--
 	Preload images for previous and next footer navigation buttons
-	
-	<div id="preloaded-images">
-	   <img src="img/previous_selected.png" width="1" height="1" alt="Previous" />
-	   <img src="img/next.png" width="1" height="1" alt="Next" />
-	</div>
 	-->
 	</body>
 	<script src="js/view_feed.js" type="text/javascript"></script>
-	<script src="js/view_feed_heath_new.js" type="text/javascript"></script>
 </html>
