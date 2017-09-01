38a0b8f519a944b19444f21c156e62d2b5bac9f8
Robert Piper <mrholypally@gmail.com>
2017-09-02 01:19:52 +1000

    User login first stage, currently it pulls the users and puts them into a table but clicking on a user doesn't login yet

---
+++ js/user_login.js
@@ -0,0 +1,21 @@
+function choose_user($name) {
+	alert($name);
+}
+
+function filter() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchBar");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("userNameTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
