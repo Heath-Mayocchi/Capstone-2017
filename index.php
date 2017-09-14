b01f242625137328445a429f974307b3a2756e7c
Heath-Mayocchi <hmayocchi@gmail.com>
2017-09-15 06:16:36 +1000

    Index all buttons goto user select page, for client meeting

--- index.php
+++ index.php
@@ -12,16 +12,16 @@
 	<section>
 		<p id="label">Select the button with the letter of your first or last name</p>
 		<div id="mainBox">
-			<button class="letters row1" id="abc" autofocus>A B C</button>
-			<button class="letters row1" id="def">D E F</button>
-			<button class="letters row1" id="ghi">G H I</button>
-			<button class="letters row1" id="jkl">J K L</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row1" id="abc" autofocus>A B C</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row1" id="def">D E F</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row1" id="ghi">G H I</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row1" id="jkl">J K L</button>
 		</div>
 		<div id="mainBox2">
-			<button class="letters row2" id="mno">M N O</button>
-			<button class="letters row2" id="pqrs">P Q R S</button>
-			<button class="letters row2" id="tuv">T U V</button>
-			<button class="letters row2" id="wxyz">W X Y Z</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row2" id="mno">M N O</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row2" id="pqrs">P Q R S</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row2" id="tuv">T U V</button>
+			<button onclick="window.location.href='user_login.php'" class="letters row2" id="wxyz">W X Y Z</button>
 		</div>
 	</section>
 	<script type="text/javascript" src="js/user_login_letter.js"></script>
