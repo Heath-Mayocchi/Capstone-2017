8a295f9d76013da86e370701e1acf98aff1e35fd
Heath-Mayocchi <hmayocchi@gmail.com>
2017-09-14 21:46:00 +1000

    User login - select surname letter frontend #100 (CSS fixes)

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SNAP LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="css/user_login_letter.css">
+</head>
+<body class="wrapper">
+	<header>
+		<h2>LOGIN</h2>
+	</header>
+
+	<section>
+		<p id="label">Select the button with the letter of your first or last name</p>
+		<div id="mainBox">
+			<button class="letters row1" id="abc" autofocus>A B C</button>
+			<button class="letters row1" id="def">D E F</button>
+			<button class="letters row1" id="ghi">G H I</button>
+			<button class="letters row1" id="jkl">J K L</button>
+		</div>
+		<div id="mainBox2">
+			<button class="letters row2" id="mno">M N O</button>
+			<button class="letters row2" id="pqrs">P Q R S</button>
+			<button class="letters row2" id="tuv">T U V</button>
+			<button class="letters row2" id="wxyz">W X Y Z</button>
+		</div>
+	</section>
+	<script type="text/javascript" src="js/user_login_letter.js"></script>
+</body>
+</html>
